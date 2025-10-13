65a00d4bc114453fac0b1a4b120e9855673c5847
vrishabhverma2210-cyber <vrishabhverma2210@gmail.com>
2025-10-13 13:49:22 +0000

    datatypes and variables

---
+++ 01_basics/01_dataypes.js
@@ -0,0 +1,33 @@
+"use strict";  // now due to this line javascript will treat your whole code as a newer version of it
+
+//alert(3+3) // error marega because we are using nodejs ,not browser(browser mai chalyenge toh chal jayega)
+
+ // code ko readibilty ko priority deni hai(imp)
+ // mostly times ; nhi use krte in javscript
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; // undefined hai yeh filhal
+
+// number => 2 to power 53
+// bigint(jab jada bda range assign krni ho , bohot kum use krte hain hum yeh , stocks vgra ke liye hota hai)
+
+// some of the daratypes and null(object hai)
+// string => ""
+// boolean => true/false
+// null => standalone value(object type hoti hai iski)
+// undefined => (jab kuch value assgin na kri ho like upr kiya tha)
+// symbol => unique
+
+
+// object(aage pdhenge detail mai)
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
