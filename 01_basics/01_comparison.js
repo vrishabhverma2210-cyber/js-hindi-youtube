af75feafa65b411c92041c225a9b17499a067479
vrishabhverma2210-cyber <vrishabhverma2210@gmail.com>
2025-10-17 02:27:38 +0000

    changes v,c,c,datasummary

---
+++ 01_basics/01_comparison.js
@@ -0,0 +1,23 @@
+// basic true or false marenge
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+ // will treat string as a number
+// console.log("2" > 1);
+// console.log("02" > 1);
+ 
+
+console.log(null > 0); // false
+console.log(null == 0);// false
+console.log(null >= 0); // true because null,>=,<= inn cases mai behave differnet krta hai
+
+console.log(undefined == 0);//f
+console.log(undefined > 0);//   F
+console.log(undefined < 0);//F
+
+// === (comparison of value as well as datatype)
+
+console.log("2" === 2);
